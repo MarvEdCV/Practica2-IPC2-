02997edeffd172d82a1787a49e31b4e9622ec0d6
Eduardo Catalán <marvineduardocv12@gmail.com>
2020-12-20 19:29:48 -0600

    Creo tabla de habitacion y hotel con sus respectivas llaves foraneas obtenidas de las tablas independientes

--- DB-hoteles.sql
+++ DB-hoteles.sql
@@ -24,5 +24,23 @@ create table CATEGORIA(
 NoEstrellas int not null primary key
 );
 
+create table HOTEL(
+NombreHotel varchar(75) not null,
+NoEstrellas int not null,
+NombrePais varchar(50),
+primary key(NombreHotel,NoEstrellas,NombrePais),
+foreign key(NoEstrellas) references CATEGORIA(NoEstrellas),
+foreign key(NombrePais) references PAIS(NombrePais)
+);
+
+create table HABITACION(
+NoEstrellas int not null,
+tipo varchar (25) not null,
+NoHabitacion int not null,
+primary key(NoEstrellas,tipo),
+foreign key(NoEstrellas) references CATEGORIA (NoEstrellas),
+foreign key(tipo) references TIPOHABITACION(tipo)
+);
+
 
 
