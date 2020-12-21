7649b2a05918780bb06c8d93fe969b676e99c31b
Eduardo Catalán <marvineduardocv12@gmail.com>
2020-12-20 19:13:42 -0600

    Creo todas las tablas independientes, y agrego la de clientes del commit anterior (se me olvido guardar cambios antes de subir el commit anterior)

--- DB-hoteles.sql
+++ DB-hoteles.sql
@@ -1,4 +1,28 @@
 create database hoteles;
 use hoteles;
 
-create table 
+create table CLIENTE (
+No_ID int NOT NULL,
+Nombre varchar(25) NOT NULL,
+Apellido varchar(25) NOT NULL,
+primary key (No_ID)
+);
+
+create table DIAFESTIVO(
+nombreFestividad varchar(50) NOT NULL primary KEY
+);
+
+create table TIPOHABITACION(
+tipo varchar(25) NOT NULL primary key
+);
+
+create table PAIS(
+NombrePais varchar(50) not null primary key
+);
+
+create table CATEGORIA(
+NoEstrellas int not null primary key
+);
+
+
+
